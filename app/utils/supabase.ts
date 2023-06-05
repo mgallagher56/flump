da15a6eae8fdaae98dd64916957457f12dbe2191
mgallagher-reid <marc.gallagher-reid@clubspark.com>
2023-06-05 09:28:16 +0100

    fix: window error

--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -1,18 +1,30 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-const isServer = typeof window === "undefined";
+const isServer = typeof window === 'undefined';
 const mode = process.env.NODE_ENV;
-const serverUrl = mode === "test" ? process.env.SUPABASE_URL_STAGING : process.env.SUPABASE_URL;
-const clientUrl = mode === "test" ? window.env.SUPABASE_URL_STAGING : window.env.SUPABASE_URL;
-const serverAnonKey = mode === "test" ? process.env.SUPABASE_ANON_KEY_STAGING : process.env.SUPABASE_ANON_KEY;
-const clientAnonKey = mode === "test" ? window.env.SUPABASE_ANON_KEY_STAGING : window.env.SUPABASE_ANON_KEY;
 
-const supabaseUrl = isServer
-  ? serverUrl
-  : clientUrl;
+const getSeverUrl = () => {
+  if (mode === 'test') return process.env.SUPABASE_URL_STAGING;
+  return process.env.SUPABASE_URL;
+};
 
-const supabaseAnonKey = isServer
-  ? serverAnonKey
-  : clientAnonKey;
+const getClientUrl = () => {
+  if (mode === 'test') return window?.env?.SUPABASE_URL_STAGING;
+  return window?.env?.SUPABASE_URL;
+};
+
+const getServerAnonKey = () => {
+  if (mode === 'test') return process.env.SUPABASE_ANON_KEY_STAGING;
+  return process.env.SUPABASE_ANON_KEY;
+};
+
+const getClientAnonKey = () => {
+  if (mode === 'test') return window?.env?.SUPABASE_ANON_KEY_STAGING;
+  return window?.env?.SUPABASE_ANON_KEY;
+};
+
+const supabaseUrl = isServer ? getSeverUrl() : getClientUrl();
+
+const supabaseAnonKey = isServer ? getServerAnonKey() : getClientAnonKey();
 
 export default createClient(supabaseUrl, supabaseAnonKey);
