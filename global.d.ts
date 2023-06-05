bf88c18eb375ae38bef5ed2746bf35897103d28b
mgallagher-reid <marc.gallagher-reid@clubspark.com>
2023-06-05 09:24:00 +0100

    fix: supabase env variables

--- global.d.ts
+++ global.d.ts
@@ -7,8 +7,10 @@ declare global {
       NODE_ENV: string;
       SUPABASE_URL: string;
       SUPABASE_ANON_KEY: string;
+      SUPABASE_URL_STAGING: string;
+      SUPABASE_ANON_KEY_STAGING: string;
     };
   }
 }
 
-export {}
+export {};
