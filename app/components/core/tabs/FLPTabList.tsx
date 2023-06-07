08c4e713c9fb9287f451fc3d5cf34e1fb98080f7
mgallagher56 <53823855+mgallagher56@users.noreply.github.com>
2023-06-07 18:27:32 +0100

    Revert "feat: test release please"
    This reverts commit 56047924b984a53fc3f1a0cf1f23e54f978bd671.

--- app/components/core/tabs/FLPTabList.tsx
+++ app/components/core/tabs/FLPTabList.tsx
@@ -4,7 +4,7 @@ import type { TabListProps } from '@chakra-ui/react';
 import { TabList } from '@chakra-ui/react';
 
 const FLPTabList: React.FC<TabListProps> = (props) => {
-  return <TabList {...props}>{props.children}</TabList>
+  return <TabList {...props}>{props.children}</TabList>;
 };
 
 export default FLPTabList;
