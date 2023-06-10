71be5c2ea1bfa73b2546c59c0599e489a64b141c
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2023-06-10 19:52:13 +0100

    test: add suabase, utils and flp button tests

---
+++ app/components/core/buttons/FLPButton.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { fireEvent, render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+
+import FLPButton from './FLPButton';
+
+describe('FLPButton', () => {
+  it('should render correctly', () => {
+    const { container } = render(<FLPButton>FLPButton</FLPButton>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly with props', () => {
+    const { container } = render(
+      <FLPButton colorScheme="green" isDisabled preventFocusOnPress variant="outline" isLoading>
+        FLPButton
+      </FLPButton>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should call onPress when clicked', () => {
+    const onPress = vi.fn();
+    render(<FLPButton onPress={onPress}>FLPButton</FLPButton>);
+    fireEvent.click(screen.getByText('FLPButton'));
+
+    expect(onPress).toHaveBeenCalled();
+  });
+
+  it('should not call onPress when clicked and isDisabled is true', () => {
+    const onPress = vi.fn();
+    render(
+      <FLPButton onPress={onPress} isDisabled>
+        FLPButton
+      </FLPButton>
+    );
+    fireEvent.click(screen.getByText('FLPButton'));
+    expect(onPress).not.toHaveBeenCalled();
+  });
+
+  it('should not call onPress when clicked and preventFocusOnPress is true', () => {
+    const onPress = vi.fn();
+    render(
+      <FLPButton onPress={onPress} preventFocusOnPress>
+        FLPButton
+      </FLPButton>
+    );
+    fireEvent.click(screen.getByText('FLPButton'));
+    expect(onPress).toHaveBeenCalled();
+  });
+
+  it('should not call onPress when clicked and isLoading is true', () => {
+    const onPress = vi.fn();
+    render(
+      <FLPButton onPress={onPress} isLoading>
+        FLPButton
+      </FLPButton>
+    );
+    fireEvent.click(screen.getByText('FLPButton'));
+    expect(onPress).not.toHaveBeenCalled();
+  });
+});
--- app/utils/supabase.ts
+++ app/utils/supabase.ts
@@ -2,7 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const isServer = typeof window === 'undefined';
 
-const supabaseUrl = isServer ? process.env.SUPABASE_URL : window?.env?.SUPABASE_URL;
-const supabaseAnonKey = isServer ? process.env.SUPABASE_ANON_KEY : window?.env?.SUPABASE_ANON_KEY;
+export const getSupaBaseUrl = (isServer: boolean): string =>
+  isServer ? process.env.SUPABASE_URL : window?.env?.SUPABASE_URL;
+export const getSupabaseAnonKey = (isServer: boolean): string =>
+  isServer ? process.env.SUPABASE_ANON_KEY : window?.env?.SUPABASE_ANON_KEY;
 
-export default createClient(supabaseUrl, supabaseAnonKey);
+export default createClient(getSupaBaseUrl(isServer), getSupabaseAnonKey(isServer));
