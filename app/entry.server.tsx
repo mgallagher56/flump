212a7715935dd4681fd3b9d8b9f98061c3b005b4
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2023-06-13 01:46:55 +0100

    feat: add i18n

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,40 +4,54 @@ import type { EntryContext } from '@remix-run/node';
 import { Response } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
 import isbot from 'isbot';
+import { resolve } from 'node:path';
 import { renderToPipeableStream } from 'react-dom/server';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { PassThrough } from 'stream';
 
+import { createInstance } from 'i18next';
+import Backend from 'i18next-fs-backend';
+
 import createEmotionCache from './createEmotionCache';
+import i18n from './i18n';
+import i18next from './i18n.server';
 
 const ABORT_DELAY = 5000;
 
-const handleRequest = (
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-): Promise<unknown> =>
-  isbot(request.headers.get('user-agent'))
-    ? handleBotRequest(request, responseStatusCode, responseHeaders, remixContext)
-    : handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext);
-export default handleRequest;
+): Promise<unknown> {
+  const callbackName = isbot(request.headers.get('user-agent')) ? 'onAllReady' : 'onShellReady';
 
-const handleBotRequest = (
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-): Promise<unknown> =>
-  new Promise((resolve, reject) => {
+  const instance = createInstance();
+  const lng = await i18next.getLocale(request);
+  const ns = i18next.getRouteNamespaces(remixContext);
+
+  await instance
+    .use(initReactI18next)
+    .use(Backend)
+    .init({
+      ...i18n,
+      lng,
+      ns,
+      backend: { loadPath: resolve('./public/locales/{{lng}}/{{ns}}.json') }
+    });
+
+  return new Promise((resolve, reject) => {
     let didError = false;
     const emotionCache = createEmotionCache();
 
     const { pipe, abort } = renderToPipeableStream(
-      <EmotionCacheProvider value={emotionCache}>
-        <RemixServer context={remixContext} url={request.url} />
-      </EmotionCacheProvider>,
+      <I18nextProvider i18n={instance}>
+        <EmotionCacheProvider value={emotionCache}>
+          <RemixServer context={remixContext} url={request.url} />
+        </EmotionCacheProvider>
+      </I18nextProvider>,
       {
-        onAllReady: () => {
+        [callbackName]: () => {
           const reactBody = new PassThrough();
           const emotionServer = createEmotionServer(emotionCache);
 
@@ -68,50 +82,4 @@ const handleBotRequest = (
 
     setTimeout(abort, ABORT_DELAY);
   });
-
-const handleBrowserRequest = (
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-): Promise<unknown> =>
-  new Promise((resolve, reject) => {
-    let didError = false;
-    const emotionCache = createEmotionCache();
-
-    const { pipe, abort } = renderToPipeableStream(
-      <EmotionCacheProvider value={emotionCache}>
-        <RemixServer context={remixContext} url={request.url} />
-      </EmotionCacheProvider>,
-      {
-        onShellReady: () => {
-          const reactBody = new PassThrough();
-          const emotionServer = createEmotionServer(emotionCache);
-
-          const bodyWithStyles = emotionServer.renderStylesToNodeStream();
-          reactBody.pipe(bodyWithStyles);
-
-          responseHeaders.set('Content-Type', 'text/html');
-
-          resolve(
-            new Response(bodyWithStyles, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode
-            })
-          );
-
-          pipe(reactBody);
-        },
-        onShellError: (error: unknown) => {
-          reject(error);
-        },
-        onError: (error: unknown) => {
-          didError = true;
-
-          console.error(error);
-        }
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
-  });
+}
