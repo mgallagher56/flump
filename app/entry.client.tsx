2cde742573db784d2c6f33cf30c5ad9fcf1cf039
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2023-06-13 01:46:55 +0100

    feat: add i18n

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,9 +8,16 @@ import React, { startTransition, StrictMode, useState } from 'react';
 import { CacheProvider } from '@emotion/react';
 import { RemixBrowser } from '@remix-run/react';
 import { hydrateRoot } from 'react-dom/client';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { getInitialNamespaces } from 'remix-i18next';
+
+import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
 import { ClientStyleContext } from './context';
 import createEmotionCache, { defaultCache } from './createEmotionCache';
+import i18n from './i18n';
 
 interface ClientCacheProviderProps {
   children: React.ReactNode;
@@ -30,13 +37,40 @@ function ClientCacheProvider({ children }: ClientCacheProviderProps): React.Reac
   );
 }
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <ClientCacheProvider>
-        <RemixBrowser />
-      </ClientCacheProvider>
-    </StrictMode>
-  );
-});
+async function hydrate(): Promise<void> {
+  if (!i18next.isInitialized)
+    await i18next
+      .use(initReactI18next)
+      .use(LanguageDetector)
+      .use(Backend)
+      .init({
+        ...i18n,
+        ns: getInitialNamespaces(),
+        backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' },
+        detection: {
+          order: ['htmlTag'],
+          caches: []
+        }
+      });
+
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <I18nextProvider i18n={i18next}>
+        <StrictMode>
+          <ClientCacheProvider>
+            <RemixBrowser />
+          </ClientCacheProvider>
+        </StrictMode>
+      </I18nextProvider>
+    );
+  });
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
+}
