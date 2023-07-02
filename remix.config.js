3b9cc824b93fed6205d641a2365fc8dfa32c95f1
mgallagher-reid <marc.gallagherreid11@bathspa.ac.uk>
2023-07-02 20:56:28 +0100

    fix: bump remix to fix refresh loop. add https for dev server

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
+    v2_headers: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    v2_headers: true
+    v2_routeConvention: true
   },
   ignoredRouteFiles: ['**/.*', '**/*.test.{js,jsx,ts,tsx}'],
   postcss: true,
